d912f0ed78534ab5a7e41d737a8bc66e944feed1
Lauwi Meara <laurasusanne@live.nl>
2024-12-01 08:29:13 +0100

    Formatting day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,19 @@
 fun main() {
     fun getLists(input: List<List<String>>): Pair<List<Int>, List<Int>> {
-        val (firstList, secondList) = input.map{ it[0].toInt() to it[1].toInt() }.unzip()
+        val (firstList, secondList) = input.map { it[0].toInt() to it[1].toInt() }.unzip()
         return Pair(firstList, secondList)
     }
 
     fun part1(input: List<List<String>>): Long {
         val (firstList, secondList) = getLists(input)
         val pairs = firstList.sorted().zip(secondList.sorted())
-        return pairs.fold(0){acc, it -> acc + distanceTo(it.first, it.second)}
+        return pairs.fold(0) { acc, it -> acc + distanceTo(it.first, it.second) }
     }
 
     fun part2(input: List<List<String>>): Long {
         val (firstList, secondList) = getLists(input)
-        val idAppearancesFirstList = firstList.groupBy{it}.map{Pair(it.key, it.value.size)}.toMap()
-        val idAppearancesSecondList = secondList.groupBy{it}.map{Pair(it.key, it.value.size)}.toMap()
+        val idAppearancesFirstList = firstList.groupBy { it }.map { Pair(it.key, it.value.size) }.toMap()
+        val idAppearancesSecondList = secondList.groupBy { it }.map { Pair(it.key, it.value.size) }.toMap()
 
         var sum = 0L
         for (item in idAppearancesFirstList) {
@@ -24,7 +24,7 @@ fun main() {
         return sum
     }
 
-    val input = readInputAsStrings("Day01").map { it.split("   ")}
+    val input = readInputAsStrings("Day01").map { it.split("   ") }
     part1(input).println()
     part2(input).println()
 }
