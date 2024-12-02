e7dbb43c31c0c6fa630b5dd76445bb7b2365b03f
Lauwi Meara <laurasusanne@live.nl>
2024-12-02 09:18:38 +0100

    Refactor day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -5,59 +5,56 @@ fun main() {
         return numbers.first - numbers.second > 0
     }
 
-    fun isGradual(numbers: Pair<Int, Int>): Boolean {
+    fun isSmallDifference(numbers: Pair<Int, Int>): Boolean {
         return abs(numbers.first - numbers.second) in 1..3
     }
 
-    fun isSafeReport(report: List<Int>): Int {
+    fun isSafeReport(report: List<Int>): Boolean {
         val zippedReport = report.zipWithNext()
         if (!zippedReport.all { levelsAreDescending(it) } && !zippedReport.all { !levelsAreDescending(it) }) {
-            return 0
+            return false
         }
-        if (zippedReport.any { !isGradual(it) }) {
-            return 0
+        if (zippedReport.any { !isSmallDifference(it) }) {
+            return false
         }
-        return 1
+        return true
     }
 
-    fun isSafeReportPart2(report: List<Int>): Int {
-        if (isSafeReport(report) == 1) {
-            return 1
+    fun isSafeReportPart2(report: List<Int>): Boolean {
+        if (isSafeReport(report)) {
+            return true
         }
 
         val zippedReport = report.zipWithNext()
         // The report is descending if half or more of the pairs is descending
-        val reportIsDescending = zippedReport.map { if (it.first - it.second > 0) 1 else 0 }
+        val reportIsDescending = zippedReport
+            .map { if (it.first - it.second > 0) 1 else 0 }
             .reduce { acc, it -> acc + it } > (zippedReport.size / 2)
         val indexFirstNotSafePair = zippedReport.indexOfFirst {
-            (reportIsDescending && !levelsAreDescending(it)) || (!reportIsDescending && levelsAreDescending(it)) || !isGradual(
-                it
-            )
+            (reportIsDescending && !levelsAreDescending(it)) ||
+                    (!reportIsDescending && levelsAreDescending(it)) ||
+                    !isSmallDifference(it)
         }
 
         // Check if the report is safe if the first of the pair is removed
         var newReport = report.toMutableList()
         newReport.removeAt(indexFirstNotSafePair)
-        if (isSafeReport(newReport) == 1) {
-            return 1
+        if (isSafeReport(newReport)) {
+            return true
         }
 
         // Check if the report is safe if the second of the pair is removed
         newReport = report.toMutableList()
         newReport.removeAt(indexFirstNotSafePair + 1)
-        if (isSafeReport(newReport) == 1) {
-            return 1
-        }
-
-        return 0
+        return isSafeReport(newReport)
     }
 
     fun part1(input: List<List<Int>>): Int {
-        return input.fold(0) { acc, it -> acc + isSafeReport(it) }
+        return input.map { if (isSafeReport(it)) 1 else 0 }.reduce { acc, it -> acc + it }
     }
 
     fun part2(input: List<List<Int>>): Int {
-        return input.fold(0) { acc, it -> acc + isSafeReportPart2(it) }
+        return input.map { if (isSafeReportPart2(it)) 1 else 0 }.reduce { acc, it -> acc + it }
     }
 
     val input = readInputAsStrings("Day02").map { line -> line.split(" ").map { it.toInt() } }
