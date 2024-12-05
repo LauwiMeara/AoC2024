b69c77c96347491332df256b6d25808b73a1ae86
Lauwi Meara <laurasusanne@live.nl>
2024-12-05 19:58:52 +0100

    Add day05

---
+++ src/Day05.kt
@@ -0,0 +1,71 @@
+fun main() {
+    data class PageOrderingRule(val page: Int, val followingPages: List<Int>)
+
+    val newLine = System.lineSeparator()
+
+    fun getRulesAndPageUpdates(input: List<String>): Pair<Map<Int, List<Int>>, List<List<Int>>> {
+        val rules = input[0].split(newLine).map { it.split("|") }
+            .map { pages -> PageOrderingRule(pages[0].toInt(), listOf(pages[1].toInt())) }.groupBy { it.page }
+            .map { it.key to it.value.flatMap { page -> page.followingPages } }.toMap()
+        val pages = input[1].split(newLine).map { it.split(",").map { page -> page.toInt() } }
+        return Pair(rules, pages)
+    }
+
+    fun part1(input: List<String>): Int {
+        var sum = 0
+        val (rules, updates) = getRulesAndPageUpdates(input)
+        for (update in updates) {
+            var updateIsCorrect = true
+            for (i in update.indices) {
+                if (!updateIsCorrect) {
+                    break
+                }
+                val followingPages = update.subList(i, update.size)
+                for (page in followingPages) {
+                    if (rules.containsKey(page) && rules[page]!!.contains(update[i])) {
+                        updateIsCorrect = false
+                        break
+                    }
+                }
+            }
+            if (updateIsCorrect) {
+                sum += update[update.size / 2]
+            }
+        }
+        return sum
+    }
+
+    fun part2(input: List<String>): Int {
+        var sum = 0
+        val (rules, updates) = getRulesAndPageUpdates(input)
+        for (update in updates) {
+            var updateIsCorrect = true
+            for (i in update.indices) {
+                if (!updateIsCorrect) {
+                    break
+                }
+                val followingPages = update.subList(i, update.size)
+                for (page in followingPages) {
+                    if (rules.containsKey(page) && rules[page]!!.contains(update[i])) {
+                        updateIsCorrect = false
+                        break
+                    }
+                }
+            }
+            if (!updateIsCorrect) {
+                val indexPerPage = mutableListOf<Pair<Int, Int>>()
+                for (i in update.indices) {
+                    val numNextPages = rules[update[i]]?.intersect(update.toSet())?.size ?: 0
+                    indexPerPage.add(Pair(update.size - 1 - numNextPages, update[i]))
+                }
+                val correctUpdate = indexPerPage.sortedBy { it.first }.map { it.second }
+                sum += correctUpdate[correctUpdate.size / 2]
+            }
+        }
+        return sum
+    }
+
+    val input = readInputSplitByDelimiter("Day05", newLine + newLine)
+    part1(input).println()
+    part2(input).println()
+}
