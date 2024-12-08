4f3ee366890510ed41c55a7319bf034381e0ab73
Lauwi Meara <laurasusanne@live.nl>
2024-12-08 07:31:46 +0100

    Add day08

--- src/Grid2D.kt
+++ src/Grid2D.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 data object Grid2D {
     enum class Direction {
         NORTH,
@@ -20,6 +22,16 @@ data object Grid2D {
     data class Position(val x: Int, val y: Int) {
         operator fun plus(other: Position): Position =
             Position(x + other.x, y + other.y)
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Position) return false
+            return this.x == other.x && this.y == other.y
+        }
+
+        override fun hashCode(): Int {
+            return Objects.hash(x, y)
+        }
     }
 
     val cardinals = mapOf(
