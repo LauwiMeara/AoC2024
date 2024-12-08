d468b8142729ae1972048bb8fa6df9896ff6281c
Lauwi Meara <laurasusanne@live.nl>
2024-12-08 19:33:23 +0100

    Add AOC dark green color

--- src/VisualizationUtils.kt
+++ src/VisualizationUtils.kt
@@ -1,4 +1,5 @@
 const val AOC_BACKGROUND_COLOR = "#0f0f23"
 const val AOC_COLOR_LIGHT_GREEN = "#00cc00"
+const val AOC_COLOR_DARK_GREEN = "#009900"
 const val POINT_SIZE_DAY_06 = 6
 const val INTERVAL_DAY_06 = 1L
