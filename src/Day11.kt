5edeb2a1e9c076fc11b8808387e71172d3fec950
Lauwi Meara <laurasusanne@live.nl>
2024-12-11 12:29:40 +0100

    Add day11.2

--- src/Day11.kt
+++ src/Day11.kt
@@ -11,42 +11,41 @@ fun main() {
         return stone
     }
 
-    fun getNewStones(stones: List<String>): List<String> {
-        val newStones = mutableListOf<String>()
+    fun getNewStones(stones: Map<String, Long>): Map<String, Long> {
+        val newStones = mutableMapOf<String, Long>()
         for (stone in stones) {
-            if (stone == "0") {
-                newStones.add("1")
-            } else if (stone.length % 2 == 0) {
-                val firstStone = stone.substring(0, stone.length / 2)
-                val secondStone = stone.substring(stone.length / 2, stone.length)
-                newStones.add(firstStone)
-                newStones.add(trimLeadingZeroes(secondStone))
+            if (stone.key == "0") {
+                newStones["1"] = if (newStones["1"] == null) stone.value else newStones["1"]!! + stone.value
+            } else if (stone.key.length % 2 == 0) {
+                val firstStone = stone.key.substring(0, stone.key.length / 2)
+                val secondStone = trimLeadingZeroes(stone.key.substring(stone.key.length / 2, stone.key.length))
+                newStones[firstStone] =
+                    if (newStones[firstStone] == null) stone.value else newStones[firstStone]!! + stone.value
+                newStones[secondStone] =
+                    if (newStones[secondStone] == null) stone.value else newStones[secondStone]!! + stone.value
             } else {
-                val newStone = (stone.toLong() * 2024).toString()
-                newStones.add(newStone)
+                val newStone = (stone.key.toLong() * 2024).toString()
+                newStones[newStone] =
+                    if (newStones[newStone] == null) stone.value else newStones[newStone]!! + stone.value
             }
         }
         return newStones
     }
 
-    fun part1(input: List<String>): Int {
-        var newStones = input
+    fun part1(input: List<String>): Long {
+        var newStones = input.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
         for (blink in 1..25) {
             newStones = getNewStones(newStones)
         }
-        return newStones.size
+        return newStones.map { it.value }.reduce { acc, it -> acc + it }
     }
 
     fun part2(input: List<String>): Long {
-        var sum = 0L
-        for (stone in input) {
-            var newStones = listOf(stone)
-            for (blink in 1..75) {
-                newStones = getNewStones(newStones)
-            }
-            sum += newStones.size
+        var newStones = input.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+        for (blink in 1..75) {
+            newStones = getNewStones(newStones)
         }
-        return sum
+        return newStones.map { it.value }.reduce { acc, it -> acc + it }
     }
 
     val input = readInputSplitByDelimiter("Day11", " ")
