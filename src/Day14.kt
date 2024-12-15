8604c182ef8d2c01d0a1013cd44c3fd5bc1d031f
Lauwi Meara <laurasusanne@live.nl>
2024-12-15 06:13:55 +0100

    Add day14.2

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,5 +1,5 @@
 fun main() {
-    data class Robot(val position: Grid2D.Position, val velocity: Grid2D.Position)
+    data class Robot(val position: Grid2D.LongPosition, val velocity: Grid2D.LongPosition)
 
     fun getRobots(input: List<String>): List<Robot> {
         val robots = mutableListOf<Robot>()
@@ -9,19 +9,18 @@ fun main() {
                     .substringAfter("p=")
                     .substringBefore(" ")
                     .split(",")
-                    .let { Grid2D.Position(it[0].toInt(), it[1].toInt()) }
+                    .let { Grid2D.LongPosition(it[0].toLong(), it[1].toLong()) }
             val velocity =
                 line.substringAfter("v=")
                     .split(",")
-                    .let { Grid2D.Position(it[0].toInt(), it[1].toInt()) }
+                    .let { Grid2D.LongPosition(it[0].toLong(), it[1].toLong()) }
             robots.add(Robot(position, velocity))
         }
         return robots
     }
 
-    fun getNewPositions(robots: List<Robot>, maxX: Int, maxY: Int): List<Grid2D.Position> {
-        val secondsWaiting = 100
-        val newPositions = mutableListOf<Grid2D.Position>()
+    fun getNewPositions(robots: List<Robot>, maxX: Int, maxY: Int, secondsWaiting: Long): List<Grid2D.LongPosition> {
+        val newPositions = mutableListOf<Grid2D.LongPosition>()
         for (robot in robots) {
             val positionWithoutGrid = robot.position + (robot.velocity * secondsWaiting)
             var newX = positionWithoutGrid.x % maxX
@@ -32,13 +31,13 @@ fun main() {
             if (newY < 0) {
                 newY += maxY
             }
-            val newPosition = Grid2D.Position(newX, newY)
+            val newPosition = Grid2D.LongPosition(newX, newY)
             newPositions.add(newPosition)
         }
         return newPositions
     }
 
-    fun getMultipliedAppearancesPerQuadrant(positions: List<Grid2D.Position>, maxX: Int, maxY: Int): Int {
+    fun getMultipliedAppearancesPerQuadrant(positions: List<Grid2D.LongPosition>, maxX: Int, maxY: Int): Int {
         val quadrantXLength = maxX / 2
         val quadrantYLength = maxY / 2
         val quadrantTopLeft = positions.count { it.x in 0..<quadrantXLength && it.y in 0..<quadrantYLength }
@@ -51,19 +50,40 @@ fun main() {
         return quadrantTopLeft * quadrantTopRight * quadrantBottomLeft * quadrantBottomRight
     }
 
+    fun printGrid(positions: List<Grid2D.LongPosition>, maxX: Int, maxY: Int) {
+        for (y in 0L..maxY) {
+            for (x in 0L..maxX) {
+                if (positions.contains(Grid2D.LongPosition(x, y))) print('*')
+                else print('.')
+            }
+            println()
+        }
+    }
+
     fun part1(input: List<String>): Int {
         val robots = getRobots(input)
         val maxX = 101
         val maxY = 103
-        val newPositions = getNewPositions(robots, maxX, maxY)
+        val secondsWaiting = 100L
+        val newPositions = getNewPositions(robots, maxX, maxY, secondsWaiting)
         return getMultipliedAppearancesPerQuadrant(newPositions, maxX, maxY)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>) {
+        val robots = getRobots(input)
+        val maxX = 101
+        val maxY = 103
+        for (secondsWaiting in 0..10000L) {
+            val newPositions = getNewPositions(robots, maxX, maxY, secondsWaiting)
+            if (newPositions.groupingBy { it.y }.eachCount().any { it.value > 30 } &&
+                newPositions.groupingBy { it.x }.eachCount().any { it.value > 30 }) {
+                println("Waiting $secondsWaiting seconds")
+                printGrid(newPositions, maxX, maxY)
+            }
+        }
     }
 
     val input = readInputAsStrings("Day14")
     part1(input).println()
-//    part2(input).println()
+    part2(input)
 }
